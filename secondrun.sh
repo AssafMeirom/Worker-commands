0dea3f8ad3331cefb54e52ea0316862b5659436c
AssafMeirom <50071206+AssafMeirom@users.noreply.github.com>
2020-05-24 20:19:10 +0300

    Update secondrun.sh

--- secondrun.sh
+++ secondrun.sh
@@ -15,5 +15,5 @@ sudo systemctl restart kubelet
 sudo sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service sshd restart
-echo -e "ubuntu\nubuntu" | passwd ubuntu
-echo -e "ubuntu\nubuntu" | passwd root
+echo -e "ubuntu\nubuntu" | sudo passwd ubuntu
+echo -e "ubuntu\nubuntu" | sudo passwd root
