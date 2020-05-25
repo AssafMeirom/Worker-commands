0bc985cb8bac689400de8d193224a314f560eed3
AssafMeirom <50071206+AssafMeirom@users.noreply.github.com>
2020-05-25 14:50:20 +0300

    Add files via upload

---
+++ WorkerCommands2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sh -x /home/ubuntu/yoni.sh
+sudo echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+sudo mkdir /data 
+sudo chmod  -R 777 /data
+sudo mkdir /bitnami
+sudo cd /bitnami
+sudo mkdir mariadb
+sudo mkdir wordpress
+sudo chmod -R 777 /bitnami/
